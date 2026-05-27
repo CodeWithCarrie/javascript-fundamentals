c00347f212de061d1fa4697894554b57b9009652
Carolista <cjones.renaissancewoman@gmail.com>
2026-05-26 21:13:17 -0400

    removed comments from demo starter

--- tutorials/03-errors-and-debugging/exception-handling/exercises/demo.js
+++ tutorials/03-errors-and-debugging/exception-handling/exercises/demo.js
@@ -1,25 +1,5 @@
 /**** EXCEPTION HANDLING ****/
 
-/*
-    Up until now, your code has executed in a perfect environment. However, in software 
-    development, unexpected issues occur—such as trying to use a system tool that doesn't 
-    exist or evaluating bad data. When JavaScript encounters an unhandled problem, it throws 
-    a fatal error, instantly crashes the program, and refuses to run any remaining code 
-    below it.
-
-    To prevent these total program shutdowns, you can build a structural safety net using 
-    three connected code blocks: try, catch, and finally:
-        - try (The Test Chamber): You wrap the code you want to test inside this block. 
-        JavaScript will attempt to execute it line-by-line.
-        - catch (The Backup Net): If anything breaks inside the try block, JavaScript 
-        immediately stops execution there and jumps straight down into the catch block. 
-        This block receives an Error Object containing details about what went wrong, 
-        allowing you to handle the error gracefully without crashing the system.
-        - finally (The Guarantee): This optional block runs at the very end, no matter what
-         happened. Whether the code ran perfectly or completely broke, the finally block 
-         always executes its instructions.
-*/
-
 /** EXAMPLE 1 - DEFENDING AGAINST A FATAL ACCIDENT **/
 
 // Let's create a simulated environment switch
